e6b6346cbc690eeb48011a9a1a7cb9140f605fca
Rob <rob@askrob.guru>
2021-11-10 20:35:07 -0600

    Lecture Code Week 7

---
+++ week_7/script.js
@@ -0,0 +1,41 @@
+let pineapple = 3
+console.log(pineapple)
+pineapple = pineapple + 23
+pineapple += 23
+console.log(pineapple);
+
+pineapple = pineapple + 1
+pineapple += 1
+pineapple++
+
+// Numbers
+var num1 = 5
+var num2 = 35
+var num3 = num1 * num2
+console.log("Total Bill: ", num3);
+
+// Booleans
+
+var bool = true
+var bool = false
+console.log(bool);
+
+// Strings
+let str1 = "Rob"
+let str2 = " eats many pizza's"
+console.log(str1);
+console.log(str1 + str2);
+
+
+
+// Arrays
+
+let arr = ["Rob", "Yearling", 29]
+//index      0        1       2
+console.log(arr);
+console.log(arr[1]);
+arr.push("Oak Park", "IL")
+console.log(arr);
+arr.pop()
+console.log(arr);
+console.log(arr.length-1);
