5643ecd391e97eabd7d07fba1b8d135d81e07b81
Rob <rob@askrob.guru>
2021-12-01 20:32:50 -0600

    Python Week 1 Lecture Code

---
+++ python_week_1/class_week_1.py
@@ -0,0 +1,101 @@
+# var x = 2
+
+# console.log(x)
+
+# Vaiable
+# x = 2
+# print(x)
+
+# Code Block
+
+# if ( x < 3) {
+#     console.log(x)
+# }
+
+# x = 2
+# i = 5
+
+# if x < 3:
+#     print(x)
+
+# if i > 3:
+#     pass
+
+# -------------------------
+
+# Data Type
+
+# Booleans
+
+# is_hungry = True
+# is_hungry = False
+
+# Integer
+# Int
+some_int = 35
+# Float
+some_float = 3.14
+# Complex
+# complex = 3j
+
+
+# String
+# restaurant = "Taco Bell"
+# reviews = 1.37
+# # print(restaurant)
+# # arr.length
+# # print(len(restaurant))
+# print(f"{restaurant} has a rating of {reviews} stars in Delware and Wyoming")
+
+"""
+age = "32"
+print(age + '1')
+print(int(age) + 1)
+print(age + str(1))
+"""
+
+# Composite Data Type
+
+# empty_list = [] # List (Array)
+
+# ninjas = ["Corey", "Max", "Leah"]
+# print(ninjas[2])
+# ninjas[0] = 'Andre'
+# print(ninjas)
+# ninjas.append('Sebuh')
+# print(ninjas)
+# ninjas.remove('Max')
+# print(ninjas)
+# ninjas.pop(1)
+# print(ninjas)
+
+# tuple_data = ('Date Of Birth', 'GPS')
+
+# some_dictionary = {
+#     "Key" : "Value",
+#     "North" : (41,31,16)
+# }
+
+# *** Loops ***
+
+# for ( i = 0; i < 100; i++) {
+#     console.log(i)
+# }
+
+for i in range(0,101,2):
+    print(i)
+# 0 Represents where to Start
+# 100 Represents where to Stop
+# 2 Represents Increment or Step
+
+for i in range(0,101):
+    print(i)
+# 0 Represents where to Start
+# 100 Represents where to Stop
+# Increment of 1 is implied
+
+for i in range(101):
+    print(i)
+# Implies we starting at 0
+# 101 represents our stop
+# Increment of 1 is implied
